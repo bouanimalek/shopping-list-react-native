80b6220320af27f5494cf2188237fbc6f9ed3cbe
malzak <malzovich@gmail.com>
2022-02-06 11:13:07 +0100

    add and show tghe products list

--- App.js
+++ App.js
@@ -1,7 +1,68 @@
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  const [product, setProduct] = useState("");
+
+  const [myProducts, setMyProducts] = useState([]);
+
+  const inputHandler = (val) => {
+    setProduct(val);
+  };
+
+  const submitHandler = () => {
+    setMyProducts((currentMyProducts) => [...currentMyProducts, product]);
+    console.log(myProducts);
+    setProduct("");
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Nouveau produit"
+          onChangeText={inputHandler}
+          value={product}
+        />
+        <Button title="Valider" onPress={submitHandler} />
+      </View>
+      <View style={styles.items}>
+        {myProducts.map((product, index) => {
+          return (
+            <Text key={index} style={styles.element}>
+              {product}
+            </Text>
+          );
+        })}
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 40,
+    paddingTop: 60,
+  },
+  inputContainer: {
+    flexDirection: "row",
+  },
+  textInput: {
+    borderColor: "grey",
+    borderWidth: 1,
+    padding: 5,
+    paddingLeft: 9,
+    fontSize: 18,
+    flexGrow: 1,
+  },
+  items: {
+    marginTop: 10,
+  },
+  element: {
+    backgroundColor: "#ffb6c1",
+    padding: 20,
+    fontSize: 18,
+    marginVertical: 5,
+  },
+});
